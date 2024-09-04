69ca2aca24ffce4a86bff2e5c8f4f4329ab2f851
SilviaSitorus <silviaeklesiana12@gmail.com>
2024-09-04 21:27:10 +0700

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,12 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24004 - Silvia Eklesiana Br Sitorus
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Selamat Datang di Objek Buku Ucok dan Butet");
+    }
+}
+
 
